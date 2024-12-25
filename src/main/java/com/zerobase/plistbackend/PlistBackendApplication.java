5f005986695fdb9a24f8537390f99d4353134650
= <today6215@gmail.com>
2024-12-25 12:49:46 +0900

    first commit

---
+++ src/main/java/com/zerobase/plistbackend/PlistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.plistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlistBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/plistbackend/PlistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.plistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
