d290a4093924b566f0877497ecbba1c490a9946c
박철현 <park82602@gmail.com>
2024-12-30 19:59:51 +0900

    refactor : 지역 변수 상수화 및 SockJS 코드 추가

--- src/main/java/com/zerobase/plistbackend/common/websocket/config/WebSocketConfig.java
+++ src/main/java/com/zerobase/plistbackend/common/websocket/config/WebSocketConfig.java
@@ -12,18 +12,25 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
-    }
+  private static final String[] SUBSCRIBE_PREFIX = {"/sub"};
+  private static final String[] PUBLISH_PREFIX = {"/pub"};
+  private static final String[] STOMP_CONNECT_URLS = {"/ws-connect"};
+  private static final String[] ALLOWED_ORIGINS_URLS = {"http://localhost:3000"};
 
-    /**
-     * setAllowedOrigins: React Port 3000
-     */
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-connect")
-                .setAllowedOrigins("http://localhost:3000");
-    }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker(SUBSCRIBE_PREFIX);
+    registry.setApplicationDestinationPrefixes(PUBLISH_PREFIX);
+  }
+
+  /**
+   * setAllowedOrigins: React Port 3000
+   */
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint(STOMP_CONNECT_URLS)
+        .setAllowedOrigins(ALLOWED_ORIGINS_URLS)
+        .withSockJS();
+  }
 }
