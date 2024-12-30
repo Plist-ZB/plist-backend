5dbd4b238d5f60da4a7f1f87775be72d03de7b68
박철현 <park82602@gmail.com>
2024-12-31 00:53:24 +0900

    feat : JPA가 UserPlaylist.java의 필드에 있는 Video 객체를 객체 타입이 아닌 JSON 혹은 TEXT 타입으로 DB와 매핑이 되려면 Converter가 있어야 해서 추가

---
+++ src/main/java/com/zerobase/plistbackend/module/userplaylist/util/VideoConverter.java
@@ -0,0 +1,31 @@
+package com.zerobase.plistbackend.module.userplaylist.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.plistbackend.module.userplaylist.model.Video;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class VideoConverter implements AttributeConverter<Video, String> {
+
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @Override
+  public String convertToDatabaseColumn(Video video) {
+    try {
+      return mapper.writeValueAsString(video);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException("Could not convert Video to JSON", e);
+    }
+  }
+
+  @Override
+  public Video convertToEntityAttribute(String json) {
+    try {
+      return mapper.readValue(json, Video.class);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException("Error converting JSON to Video", e);
+    }
+  }
+}
