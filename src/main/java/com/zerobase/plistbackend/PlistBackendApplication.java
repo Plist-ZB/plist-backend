c3b830793705c09e0901a9d7fc585467ba775037
박철현 <park82602@gmail.com>
2025-01-02 19:33:45 +0900

    style : 사용되지 않는 애노테이션 삭제

--- src/main/java/com/zerobase/plistbackend/PlistBackendApplication.java
+++ src/main/java/com/zerobase/plistbackend/PlistBackendApplication.java
@@ -1,14 +1,9 @@
 package com.zerobase.plistbackend;
 
-import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
-@EntityListeners(AuditingEntityListener.class)
 public class PlistBackendApplication {
 
     public static void main(String[] args) {
