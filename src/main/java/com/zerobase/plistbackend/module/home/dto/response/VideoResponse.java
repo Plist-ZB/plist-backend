304e6f0406692ebc4d7851bda12b6e8b23b00a64
박철현 <park82602@gmail.com>
2025-01-03 17:12:50 +0900

    refactor : Setter 애노테이션 추가, Builder 애노테이션을 사용하기 위한 NoArgsConstrutor, AllArgsConstructor 추가

--- src/main/java/com/zerobase/plistbackend/module/home/dto/response/VideoResponse.java
+++ src/main/java/com/zerobase/plistbackend/module/home/dto/response/VideoResponse.java
@@ -1,12 +1,14 @@
 package com.zerobase.plistbackend.module.home.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VideoResponse {
 
   private Long id;
