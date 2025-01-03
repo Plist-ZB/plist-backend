c128413366d72753eb6f817364142f8fe5e8427e
박철현 <park82602@gmail.com>
2025-01-03 17:33:49 +0900

    feat : JsonParsing 과정에서 일어날 수 있는 IOException을 한곳에서 처리하기 위해 AOP 적용

--- src/main/java/com/zerobase/plistbackend/common/app/config/YouTubeApiProperties.java
+++ src/main/java/com/zerobase/plistbackend/common/app/config/YouTubeApiProperties.java
@@ -1,12 +1,26 @@
 package com.zerobase.plistbackend.common.app.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.plistbackend.common.app.aop.TryCatch;
+import com.zerobase.plistbackend.module.home.dto.response.VideoResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Slf4j
 @Getter
 @ConfigurationProperties("youtube-api")
 public class YouTubeApiProperties {
+
   private final String url;
+  private final String selectVideoUrl;
   private final String type;
   private final String part;
   private final long maxResults;
@@ -15,10 +29,11 @@ public class YouTubeApiProperties {
   private final String videoEmbeddable;
   private final String apiKey;
 
-  public YouTubeApiProperties(String url, String type, String part,
-      long maxResults, String order, String relevanceLanguage,
-      String videoEmbeddable, String apiKey) {
+  public YouTubeApiProperties(String url, String selectVideoUrl, String type, String part,
+      long maxResults, String order, String relevanceLanguage, String videoEmbeddable,
+      String apiKey) {
     this.url = url;
+    this.selectVideoUrl = selectVideoUrl;
     this.type = type;
     this.part = part;
     this.maxResults = maxResults;
@@ -26,5 +41,48 @@ public class YouTubeApiProperties {
     this.relevanceLanguage = relevanceLanguage;
     this.videoEmbeddable = videoEmbeddable;
     this.apiKey = apiKey;
+  }
+
+  @TryCatch
+  private String getVideoAsString(String videoId, ObjectMapper objectMapper) throws IOException {
+    URL selectOneQueryURL = new URL(String.format(this.selectVideoUrl, this.getApiKey(), videoId));
+
+    BufferedReader br = new BufferedReader(
+        new InputStreamReader(selectOneQueryURL.openStream(), StandardCharsets.UTF_8));
+
+    StringBuilder sb = new StringBuilder();
+
+    String inputLine;
+    while ((inputLine = br.readLine()) != null) {
+      sb.append(inputLine);
+    }
+
+    return objectMapper.writerWithDefaultPrettyPrinter()
+        .writeValueAsString(objectMapper.readTree(sb.toString()));
+  }
+
+  @TryCatch
+  public VideoResponse createVideoResponse(String videoId, ObjectMapper objectMapper)
+      throws IOException {
+    String videoAsString = getVideoAsString(videoId, objectMapper);
+
+    JsonNode rootNode = objectMapper.readTree(videoAsString);
+    JsonNode itemsNode = rootNode.get("items");
+
+    VideoResponse response = new VideoResponse();
+    for (JsonNode itemNode : itemsNode) {
+      JsonNode idNode = itemNode.get("id");
+      JsonNode snippetNode = itemNode.get("snippet");
+      JsonNode thumbnailsNode = snippetNode.get("thumbnails");
+      JsonNode thumbnailDefaultNode = thumbnailsNode.get("default");
+
+      response = VideoResponse.builder()
+          .videoId(idNode.asText())
+          .videoName(snippetNode.get("title").asText())
+          .videoThumbnail(thumbnailDefaultNode.get("url").asText())
+          .videoDescription(snippetNode.get("description").asText())
+          .build();
+    }
+    return response;
   }
 }
