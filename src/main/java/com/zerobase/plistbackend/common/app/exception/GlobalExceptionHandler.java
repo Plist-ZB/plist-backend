9aa16625b84092283d8ce2cd152b7a817da2a70d
박철현 <park82602@gmail.com>
2025-01-04 16:05:28 +0900

    feat : UserPlaylistException을 처리할 GlobalExceptionHandler 추가

--- src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.zerobase.plistbackend.common.app.exception;
 
+import com.zerobase.plistbackend.module.userplaylist.exception.UserPlaylistException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,6 +9,11 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(JsonParseException.class)
   public ErrorResponse handleJsonParseException(JsonParseException e) {
+    return ErrorResponse.create(e.getErrorStatus());
+  }
+
+  @ExceptionHandler(UserPlaylistException.class)
+  public ErrorResponse handleJsonParseException(UserPlaylistException e) {
     return ErrorResponse.create(e.getErrorStatus());
   }
 }
