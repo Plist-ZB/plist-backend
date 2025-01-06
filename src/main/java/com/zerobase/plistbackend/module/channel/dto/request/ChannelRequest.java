1feb5bde5016f3014d170dd3b625edaddbd52f38
HAKSUYOON <hakssu93@gmail.com>
2025-01-07 00:57:24 +0900

    style : 사용하지 않는 import문 삭제

--- src/main/java/com/zerobase/plistbackend/module/channel/dto/request/ChannelRequest.java
+++ src/main/java/com/zerobase/plistbackend/module/channel/dto/request/ChannelRequest.java
@@ -1,18 +1,14 @@
 package com.zerobase.plistbackend.module.channel.dto.request;
 
-import com.zerobase.plistbackend.module.playlist.entity.Playlist;
-import com.zerobase.plistbackend.module.userplaylist.entity.UserPlaylist;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Range;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChannelRequest {
 
-  @Range(min=1, max = 50)
   private String channelName;
   private Long userPlaylistId;
   private String channelCategory;
