7f4bac75569961d679131a1ebbc7ed110f051497
HAKSUYOON <hakssu93@gmail.com>
2025-01-07 00:52:40 +0900

    feat : 참가자 엔티티 생성자 추가 - 호스트 참가자 생성 - 사용자 참가자 생성

--- src/main/java/com/zerobase/plistbackend/module/participant/entity/Participant.java
+++ src/main/java/com/zerobase/plistbackend/module/participant/entity/Participant.java
@@ -4,6 +4,7 @@ import com.zerobase.plistbackend.module.channel.entity.Channel;
 import com.zerobase.plistbackend.module.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,12 +19,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @Table(name = "participant")
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Participant {
 
@@ -46,6 +49,22 @@ public class Participant {
   @CreatedDate
   @Column(name = "participant_created_at")
   private Timestamp participantCreatedAt;
+
+  public static Participant host(User user, Channel channel) {
+    return Participant.builder()
+        .user(user)
+        .channel(channel)
+        .isHost(true)
+        .build();
+  }
+
+  public static Participant viewer(User user, Channel channel) {
+    return Participant.builder()
+        .user(user)
+        .channel(channel)
+        .isHost(false)
+        .build();
+  }
 
   public Boolean getHostStatus() {
     return isHost;
