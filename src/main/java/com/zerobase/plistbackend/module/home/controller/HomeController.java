27864c2f947036c7beeeebae662dc4114022c6ae
HAKSUYOON <hakssu93@gmail.com>
2025-01-06 21:04:21 +0900

    refactor : API 주소 변경

--- src/main/java/com/zerobase/plistbackend/module/home/controller/HomeController.java
+++ src/main/java/com/zerobase/plistbackend/module/home/controller/HomeController.java
@@ -6,19 +6,21 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v3/api")
 @RequiredArgsConstructor
 public class HomeController {
 
   private final HomeService homeService;
 
-  @GetMapping("/service/search")
-  public ResponseEntity<List<VideoResponse>> searchVideo(@RequestParam("searchValue") String searchValue) {
+  @GetMapping("/search-video")
+  public ResponseEntity<List<VideoResponse>> searchVideo(@RequestParam("keyword") String keyword) {
 
-    List<VideoResponse> videoResponseList = homeService.searchVideo(searchValue);
+    List<VideoResponse> videoResponseList = homeService.searchVideo(keyword);
 
     return ResponseEntity.ok(videoResponseList);
   }
