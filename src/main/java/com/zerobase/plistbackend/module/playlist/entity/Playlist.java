0a9d68388be986398eb72110a349a562ee29410b
HAKSUYOON <hakssu93@gmail.com>
2025-01-07 00:54:48 +0900

    feat : 플레이리스트 엔티티 변경 - 채널과는 1대1 매핑으로 변경 - 비디오 컬럼이 List<Video>로 변경 - 생성자

--- src/main/java/com/zerobase/plistbackend/module/playlist/entity/Playlist.java
+++ src/main/java/com/zerobase/plistbackend/module/playlist/entity/Playlist.java
@@ -2,30 +2,32 @@ package com.zerobase.plistbackend.module.playlist.entity;
 
 import com.zerobase.plistbackend.module.channel.entity.Channel;
 import com.zerobase.plistbackend.module.playlist.util.PlaylistVideoConverter;
+import com.zerobase.plistbackend.module.userplaylist.entity.UserPlaylist;
 import com.zerobase.plistbackend.module.userplaylist.model.Video;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @Table(name = "playlist")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Playlist {
 
   @Id
@@ -33,12 +35,18 @@ public class Playlist {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long playlistId;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @OneToOne
   @JoinColumn(name = "channel_id")
   private Channel channel;
 
   @Lob
-  @Column(name = "video")
+  @Column(name = "video", columnDefinition = "LONGTEXT")
   @Convert(converter = PlaylistVideoConverter.class)
-  private Video video;
+  private List<Video> videoList;
+
+  public static Playlist from(UserPlaylist userPlaylist) {
+    return Playlist.builder()
+        .videoList(userPlaylist.getVideoList())
+        .build();
+  }
 }
