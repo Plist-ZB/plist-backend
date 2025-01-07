83bb13052da81132d8b69fcc42b8ef65f93d31bd
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-07 12:35:51 +0900

    style : 사용되지 않는 클래스 제거

--- src/main/java/com/zerobase/plistbackend/module/userplaylist/entity/UserPlaylist.java
+++ src/main/java/com/zerobase/plistbackend/module/userplaylist/entity/UserPlaylist.java
@@ -2,7 +2,7 @@ package com.zerobase.plistbackend.module.userplaylist.entity;
 
 import com.zerobase.plistbackend.module.playlist.entity.Playlist;
 import com.zerobase.plistbackend.module.user.entity.User;
-import com.zerobase.plistbackend.module.userplaylist.domain.Video;
+import com.zerobase.plistbackend.module.userplaylist.model.Video;
 import com.zerobase.plistbackend.module.userplaylist.util.UserPlaylistVideoConverter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
