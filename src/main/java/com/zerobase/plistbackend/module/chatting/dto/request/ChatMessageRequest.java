e97e85e0a81476766d471628ca6980f6fd19bf8a
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-07 13:04:58 +0900

    style : 필요없는 import 삭제

--- src/main/java/com/zerobase/plistbackend/module/chatting/dto/request/ChatMessageRequest.java
+++ src/main/java/com/zerobase/plistbackend/module/chatting/dto/request/ChatMessageRequest.java
@@ -1,14 +1,10 @@
 package com.zerobase.plistbackend.module.chatting.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChatMessageRequest {
   private String sender;
   private String message;
