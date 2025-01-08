b39bd38d0cd2a7040558d6f1346470b6f583f172
= <today6215@gmail.com>
2025-01-09 04:04:33 +0900

    feat : 유저 정보 조회 추가

--- src/main/java/com/zerobase/plistbackend/module/user/controller/UserController.java
+++ src/main/java/com/zerobase/plistbackend/module/user/controller/UserController.java
@@ -1,33 +1,37 @@
 package com.zerobase.plistbackend.module.user.controller;
 
+import com.zerobase.plistbackend.module.user.dto.response.ProfileResponse;
+import com.zerobase.plistbackend.module.user.model.auth.CustomOAuth2User;
+import com.zerobase.plistbackend.module.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/v3/api")
+@Tag(name = "User Service API", description = "회원 정보 관리")
 public class UserController {
 
+  private final UserService userService;
+
   // 테스트용 컨트롤러입니다
-
-  @GetMapping("/")
-  public String home() {
-    return "Welcome to the home page!";
-  }
-
   @GetMapping("/test")
   public String test() {
     return "Welcome to the test page!";
   }
 
-  @GetMapping("/login")
-  public String loginPage() {
-    return """
-               <html>
-               <body>
-                   <h1>Login Page</h1>
-                   <a href="/oauth2/authorization/google">Login with Google</a>
-               </body>
-               </html>
-               """;
+  @Operation(summary = "회원 정보 조회", description = "해당 회원의 정보를 조회할 수 있습니다.")
+  @GetMapping("/user/profile")
+  public ResponseEntity<ProfileResponse> findProfile(
+      @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+    return ResponseEntity.ok(userService.findProfile(customOAuth2User.findEmail()));
   }
 
 }
---
+++ src/main/java/com/zerobase/plistbackend/module/user/service/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.zerobase.plistbackend.module.user.service;
+
+import com.zerobase.plistbackend.module.user.dto.response.ProfileResponse;
+import com.zerobase.plistbackend.module.user.entity.User;
+import com.zerobase.plistbackend.module.user.jwt.JwtUtil;
+import com.zerobase.plistbackend.module.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+  private final UserRepository userRepository;
+
+  @Transactional(readOnly = true)
+  public ProfileResponse findProfile(String email) {
+    log.info("Find Profile Request email: {}", email);
+    User user = userRepository.findByUserEmail(email);
+    return new ProfileResponse(email, user.getUserName(), user.getUserImage());
+  }
+
+}
