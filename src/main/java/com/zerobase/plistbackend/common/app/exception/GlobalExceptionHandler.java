683dbfb870e663d0f24dd9aa8d2eddce4f59f00f
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-09 00:57:33 +0900

    feat : ChatException 추가

--- src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package com.zerobase.plistbackend.common.app.exception;
 import com.zerobase.plistbackend.module.channel.exception.ChannelException;
 import com.zerobase.plistbackend.module.user.exception.OAuth2UserException;
 import com.zerobase.plistbackend.module.userplaylist.exception.UserPlaylistException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -28,5 +30,4 @@ public class GlobalExceptionHandler {
   public ErrorResponse handleOAuth2UserException(OAuth2UserException e) {
     return ErrorResponse.create(e.getErrorStatus());
   }
-
 }
