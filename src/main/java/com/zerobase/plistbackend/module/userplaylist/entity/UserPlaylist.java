9fc56d59b19ac9780379bb76c3a9f40000bfb56d
HAKSUYOON <hakssu93@gmail.com>
2025-01-10 11:23:31 +0900

    refactor: 유저 플레이리스트 엔티티 수정 - 유저플레이리스트 이름 50자로 제한 - 유저플레이리스트 비디오리스트 Setter 추가, 기본값 추가 - 채널 퇴장 시, 유저플레이리스트로 저장 시 유저플레이리스트이름을 채널이름으로 저장

--- src/main/java/com/zerobase/plistbackend/module/userplaylist/entity/UserPlaylist.java
+++ src/main/java/com/zerobase/plistbackend/module/userplaylist/entity/UserPlaylist.java
@@ -2,7 +2,8 @@ package com.zerobase.plistbackend.module.userplaylist.entity;
 
 import com.zerobase.plistbackend.module.playlist.entity.Playlist;
 import com.zerobase.plistbackend.module.user.entity.User;
-import com.zerobase.plistbackend.module.userplaylist.model.Video;
+import com.zerobase.plistbackend.module.userplaylist.dto.request.UserPlaylistRequest;
+import com.zerobase.plistbackend.module.home.model.Video;
 import com.zerobase.plistbackend.module.userplaylist.util.UserPlaylistVideoConverter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
@@ -17,12 +18,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -41,27 +42,27 @@ public class UserPlaylist {
   @JoinColumn(name = "user_id")
   private User user;
 
-  @Column(name = "userplaylist_name", length = 30)
+  @Column(name = "userplaylist_name", length = 50)
   private String userPlaylistName;
 
   @Lob
+  @Setter
+  @Builder.Default
   @Column(name = "video", columnDefinition = "LONGTEXT")
   @Convert(converter = UserPlaylistVideoConverter.class)
-  private List<Video> videoList;
+  private List<Video> videoList = new ArrayList<>();
 
-  public static UserPlaylist createUserPlaylist(User user, String userPlaylistName) {
+  public static UserPlaylist createUserPlaylist(User user, UserPlaylistRequest userPlaylistRequest) {
     return UserPlaylist.builder()
         .user(user)
-        .userPlaylistName(userPlaylistName)
-        .videoList(new ArrayList<>())
+        .userPlaylistName(userPlaylistRequest.getUserPlaylistName())
         .build();
   }
 
-  public static UserPlaylist fromChannelPlaylist (User user, Playlist playlist) {
-    String uuid = UUID.randomUUID().toString();
+  public static UserPlaylist fromChannelPlaylist (User user, Playlist playlist, String channelName) {
     return UserPlaylist.builder()
         .user(user)
-        .userPlaylistName("Playlist_" + uuid)
+        .userPlaylistName(channelName)
         .videoList(playlist.getVideoList())
         .build();
   }
