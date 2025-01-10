9af03b5a770f29546c11f0fc8b9ee53a101701db
HAKSUYOON <hakssu93@gmail.com>
2025-01-10 11:21:07 +0900

    refactor: 현재 스트리밍 중인 채널 응답값 수정

--- src/main/java/com/zerobase/plistbackend/module/channel/dto/response/StreamingChannelResponse.java
+++ src/main/java/com/zerobase/plistbackend/module/channel/dto/response/StreamingChannelResponse.java
@@ -1,8 +1,6 @@
 package com.zerobase.plistbackend.module.channel.dto.response;
 
 import com.zerobase.plistbackend.module.channel.entity.Channel;
-import com.zerobase.plistbackend.module.channel.type.ChannelStatus;
-import com.zerobase.plistbackend.module.playlist.dto.response.PlaylistResponse;
 import java.time.Duration;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,26 +14,22 @@ public class StreamingChannelResponse {
   private String channelCategoryName;
   private String channelThumbnail;
   private String channelStreamingTime;
-  private ChannelStatus channelStatus;
   private String channelHost;
   private int channelParticipantCount;
-  private PlaylistResponse playlistResponse;
   //  private Long channelCapacity;
 
 
   public static StreamingChannelResponse createStreamingChannelResponse(Channel channel) {
-    PlaylistResponse playlistResponse = PlaylistResponse.from(channel.getChannelPlaylist());
 
     return StreamingChannelResponse.builder()
         .channelId(channel.getChannelId())
         .channelName(channel.getChannelName())
         .channelCategoryName(channel.getCategory().getCategoryName())
-        .channelThumbnail(channel.getChannelThumbnail())
+        .channelThumbnail(channel.getChannelPlaylist().getVideoList().get(0)
+            .getVideoThumbnail())
         .channelStreamingTime(streamingTime(channel))
-        .channelStatus(channel.getChannelStatus())
         .channelHost(channel.getChannelHost())
         .channelParticipantCount(channel.getChannelParticipants().size())
-        .playlistResponse(playlistResponse)
         .build();
   }
 
