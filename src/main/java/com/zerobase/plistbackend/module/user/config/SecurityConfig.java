9de9e503729c3e8a6b7c16780f3994cb2457c6a5
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-14 06:41:12 +0900

    style : 변수 삭제

--- src/main/java/com/zerobase/plistbackend/module/user/config/SecurityConfig.java
+++ src/main/java/com/zerobase/plistbackend/module/user/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
       "/",
       "/ws-connect/**",
       "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/error",
-      "/sc", "13.209.237.110"
+      "/sc"
   };
 
   @Bean
