5b0b43892877a2d164543e43c102deedb96a005e
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-14 19:38:18 +0900

    feat : Swagger 애노테이션 적용

--- src/main/java/com/zerobase/plistbackend/common/server/ServerCheckController.java
+++ src/main/java/com/zerobase/plistbackend/common/server/ServerCheckController.java
@@ -1,5 +1,6 @@
 package com.zerobase.plistbackend.common.server;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "ServerCheckController", description = "현재 서버 상태 확인용 API")
 public class ServerCheckController {
 
   private final PlistProperties plistProperties;
