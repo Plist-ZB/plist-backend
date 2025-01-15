75b85553fdbf5a759b9606cefaaf4674c0ba0a47
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-15 22:53:15 +0900

    style : 클래스명 변경

--- src/main/java/com/zerobase/plistbackend/module/playlist/domain/PlaylistCrudEventPublisher.java
+++ src/main/java/com/zerobase/plistbackend/module/playlist/domain/PlaylistCrudEventPublisher.java
@@ -3,19 +3,22 @@ package com.zerobase.plistbackend.module.playlist.domain;
 import com.zerobase.plistbackend.module.channel.dto.response.DetailChannelResponse;
 import com.zerobase.plistbackend.module.channel.service.ChannelService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
-public class PlaylistCrudPublisher {
+public class PlaylistCrudEventPublisher {
 
   private final ChannelService channelService;
   private final SimpMessagingTemplate messagingTemplate;
 
   @EventListener
   public void handlePlaylistUpdateEvent(PlaylistCrudEvent event) {
+    log.info("Received playlist update event: {}", event.getChannelId());
     Long destinationVariable = event.getChannelId();
     DetailChannelResponse result = channelService.findOneChannel(destinationVariable);
     messagingTemplate.convertAndSend("/sub/playlist." + destinationVariable, result.getVideoList());
