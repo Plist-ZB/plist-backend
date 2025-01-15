db7cb1d7b0deda08b2589816d9312cbfbaebf453
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-15 21:03:28 +0900

    test : 동시성 이슈 테스트 시나리오 재작성

--- src/test/java/com/zerobase/plistbackend/module/websocket/domain/VideoSyncManagerTest.java
+++ src/test/java/com/zerobase/plistbackend/module/websocket/domain/VideoSyncManagerTest.java
@@ -3,6 +3,7 @@ package com.zerobase.plistbackend.module.websocket.domain;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -13,15 +14,29 @@ class VideoSyncManagerTest {
   @Test
   void testConcurrentSyncVideo() throws InterruptedException {
     VideoSyncManager manager = new VideoSyncManager();
-    ExecutorService executor = Executors.newFixedThreadPool(10);
 
-    for (int i = 0; i < 100; i++) {
-      final Long channelId = (long) (i % 10);
-      executor.submit(() -> manager.updateCurrentTime(channelId, System.currentTimeMillis()));
+    int threadPoolSize = 32;
+    ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize);
+
+
+    int threadCount = 100;
+    CountDownLatch latch = new CountDownLatch(threadCount);
+
+    for (int i = 0; i < threadCount; i++) {
+      final Long channelId = (long) (i % 20);
+      executor.submit(() -> {
+        try {
+          System.out.println("channelId = " + channelId);
+          manager.updateCurrentTime(channelId, System.currentTimeMillis());
+        } finally {
+          latch.countDown();
+        }
+      });
     }
-
+    latch.await();
     executor.shutdown();
+
     assertTrue(executor.awaitTermination(5, TimeUnit.SECONDS));
-    assertNotNull(manager.getCurrentTime(0L));
+    assertNotNull(manager.getCurrentTime(0L)); // 문제가 발생할 경우 null 반환
   }
 }
