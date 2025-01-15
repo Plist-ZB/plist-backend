13ac9c0b4c8fa8f00305c47f709e10b9f7112e9e
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-15 18:57:26 +0900

    feat : WebSocketControllerExceptionHandler 추가

--- src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
@@ -2,11 +2,11 @@ package com.zerobase.plistbackend.common.app.exception;
 
 import com.zerobase.plistbackend.module.category.exception.CategoryException;
 import com.zerobase.plistbackend.module.channel.exception.ChannelException;
-import com.zerobase.plistbackend.module.user.exception.UserException;
 import com.zerobase.plistbackend.module.home.exception.VideoException;
 import com.zerobase.plistbackend.module.playlist.exception.PlaylistException;
-import com.zerobase.plistbackend.module.user.exception.OAuth2UserException;
+import com.zerobase.plistbackend.module.user.exception.UserException;
 import com.zerobase.plistbackend.module.userplaylist.exception.UserPlaylistException;
+import com.zerobase.plistbackend.module.websocket.exception.WebSocketControllerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -48,6 +48,11 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(VideoException.class)
   public ErrorResponse handleVideoException(VideoException e) {
+    return ErrorResponse.create(e.getErrorStatus());
+  }
+
+  @ExceptionHandler(WebSocketControllerException.class)
+  public ErrorResponse handleWebSocketControllerException(WebSocketControllerException e) {
     return ErrorResponse.create(e.getErrorStatus());
   }
 }
