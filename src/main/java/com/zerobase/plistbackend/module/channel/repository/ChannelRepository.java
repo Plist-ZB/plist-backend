8eb15c7d47074290d1f73b93e75969752058e982
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-15 16:00:36 +0900

    feat : 채널아이디와 채널의 상태로 Channel Entity 조회 메서드 추가

--- src/main/java/com/zerobase/plistbackend/module/channel/repository/ChannelRepository.java
+++ src/main/java/com/zerobase/plistbackend/module/channel/repository/ChannelRepository.java
@@ -3,6 +3,7 @@ package com.zerobase.plistbackend.module.channel.repository;
 import com.zerobase.plistbackend.module.channel.entity.Channel;
 import com.zerobase.plistbackend.module.channel.type.ChannelStatus;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -43,4 +44,7 @@ public interface ChannelRepository extends JpaRepository<Channel, Long> {
   );
 
   List<Channel> findByChannelHostAndChannelStatus(String userName, ChannelStatus channelStatus);
+
+  Optional<Channel> findByChannelIdAndChannelStatus(Long channelId,
+      ChannelStatus channelStatusActive);
 }
