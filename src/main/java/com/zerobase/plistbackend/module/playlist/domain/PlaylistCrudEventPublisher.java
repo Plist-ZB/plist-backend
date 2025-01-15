6ea8aefe2293aef2e322bdeab2eb9d0207f788f3
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-15 23:02:54 +0900

    style : Swagger 애노테이션 추가

--- src/main/java/com/zerobase/plistbackend/module/playlist/domain/PlaylistCrudEventPublisher.java
+++ src/main/java/com/zerobase/plistbackend/module/playlist/domain/PlaylistCrudEventPublisher.java
@@ -2,6 +2,7 @@ package com.zerobase.plistbackend.module.playlist.domain;
 
 import com.zerobase.plistbackend.module.channel.dto.response.DetailChannelResponse;
 import com.zerobase.plistbackend.module.channel.service.ChannelService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -16,6 +17,10 @@ public class PlaylistCrudEventPublisher {
   private final ChannelService channelService;
   private final SimpMessagingTemplate messagingTemplate;
 
+  @Operation(
+      summary = "채널의 플레이리스트에서 변경 감지 시 작동, 채널에 속하는 플레이리스트의 최신 DB정보를 가져와 참여자들에게 전달",
+      description = "Subscribe Path = {/sub/playlist.{channelId}}"
+  )
   @EventListener
   public void handlePlaylistUpdateEvent(PlaylistCrudEvent event) {
     log.info("Call Playlist Update Event : {}", event.getChannelId());
