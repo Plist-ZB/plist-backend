86a6800d5d6ea4839350d51436ccbdace6862ab2
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-20 02:33:17 +0900

    style : 사용안하는 import문 제거

--- src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/plistbackend/common/app/exception/GlobalExceptionHandler.java
@@ -1,11 +1,5 @@
 package com.zerobase.plistbackend.common.app.exception;
 
-import com.zerobase.plistbackend.module.category.exception.CategoryException;
-import com.zerobase.plistbackend.module.channel.exception.ChannelException;
-import com.zerobase.plistbackend.module.home.exception.VideoException;
-import com.zerobase.plistbackend.module.user.exception.UserException;
-import com.zerobase.plistbackend.module.userplaylist.exception.UserPlaylistException;
-import com.zerobase.plistbackend.module.websocket.exception.WebSocketControllerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
