35d132ccca58b9187a340468c8ccd6ab3ce7486c
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-21 22:56:56 +0900

    test : 그에 따른 테스트 코드 변경

--- src/test/java/com/zerobase/plistbackend/module/websocket/domain/VideoSyncManagerTest.java
+++ src/test/java/com/zerobase/plistbackend/module/websocket/domain/VideoSyncManagerTest.java
@@ -3,6 +3,11 @@ package com.zerobase.plistbackend.module.websocket.domain;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.plistbackend.module.home.model.Video;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -38,5 +43,17 @@ class VideoSyncManagerTest {
 
     assertTrue(executor.awaitTermination(5, TimeUnit.SECONDS));
     assertNotNull(manager.getCurrentTime(0L)); // 문제가 발생할 경우 null 반환
+  }
+
+  @Test
+  void convertToJsonTest () throws JsonProcessingException {
+    HashMap<String, List<Video>> map = new HashMap<>();
+    ObjectMapper mapper = new ObjectMapper();
+    map.put("PLAYLIST", List.of(new Video(1L, "asdasd", "asdasd.img", "asdasd"),
+        new Video(2L, "asdasd", "asdasd.img", "asdasd")
+        ,new Video(3L, "asdasd", "asdasd.img", "asdasd")));
+
+    String json = mapper.writeValueAsString(map);
+    System.out.println("json = " + json);
   }
 }
