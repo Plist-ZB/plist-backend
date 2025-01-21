c5f70ed7aab7451ec3a0f1be28c0f9b8e1ae60cb
chulhyun96 <125968395+chulhyun96@users.noreply.github.com>
2025-01-21 22:38:23 +0900

    refactor : VideoResponse 인터페이스 구현

--- src/main/java/com/zerobase/plistbackend/module/websocket/domain/VideoResponseJsonMap.java
+++ src/main/java/com/zerobase/plistbackend/module/websocket/domain/VideoResponseJsonMap.java
@@ -1,20 +1,18 @@
 package com.zerobase.plistbackend.module.websocket.domain;
 
 import com.zerobase.plistbackend.module.websocket.dto.response.VideoSyncResponse;
+import com.zerobase.plistbackend.module.websocket.dto.videointerface.VideoResponse;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import lombok.Getter;
-import org.springframework.stereotype.Component;
 
 @Getter
-@Component
 public class VideoResponseJsonMap {
 
-  private final Map<String, VideoSyncResponse> videoSyncResponseMap = new ConcurrentHashMap<>();
-  private static final String TYPE = "videoState";
+  private final Map<String, VideoResponse> videoDataResponseMap = new ConcurrentHashMap<>();
 
-  public void setUpData(VideoSyncResponse response) {
-    videoSyncResponseMap.put(TYPE, response);
+  public void setUpData(String type,VideoSyncResponse response) {
+    videoDataResponseMap.put(type, response);
   }
 
 }
