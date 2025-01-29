b72fc1cbe1b449b8ec501761be73284a13c2e4ea
JIHU96 <korjihu@gmail.com>
2025-01-29 14:57:22 +0900

    feat: SwaggerConfig에 로컬서버와 배포서버 명시

--- src/main/java/com/zerobase/plistbackend/common/app/config/SwaggerConfig.java
+++ src/main/java/com/zerobase/plistbackend/common/app/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,8 +23,18 @@ public class SwaggerConfig {
         .title("PLIST API")
         .description("사람과 음악을 잇다, 플리스트");
 
+    Server localServer = new Server();
+    localServer.setUrl("http://localhost:8080");
+    localServer.setDescription("로컬 서버");
+
+    Server prodServer = new Server();
+    prodServer.setUrl("https://api.plist.shop");
+    prodServer.setDescription("운영 서버");
+
     return new OpenAPI()
         .info(info)
+        .addServersItem(localServer)
+        .addServersItem(prodServer)
         .addSecurityItem(new SecurityRequirement().addList(customSchemeName))
         .components(new Components().addSecuritySchemes(customSchemeName, createAPIKeyScheme()));
   }
