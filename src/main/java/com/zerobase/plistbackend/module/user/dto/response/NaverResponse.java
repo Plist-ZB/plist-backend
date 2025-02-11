a09bbdc4692992726772ffa8acd5cce3342402fa
chulhyun96 <park82602@gmail.com>
2025-02-11 18:23:01 +0900

    feat : API 로그인 추가로 인한 로직

---
+++ src/main/java/com/zerobase/plistbackend/module/user/dto/response/NaverResponse.java
@@ -0,0 +1,38 @@
+package com.zerobase.plistbackend.module.user.dto.response;
+
+import java.util.Map;
+
+public class NaverResponse implements OAuth2Response {
+
+    private final Map<String, Object> attribute;
+
+    public NaverResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String findProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String findProviderId() {
+        return attribute.get("id").toString();
+    }
+
+    @Override
+    public String findEmail() {
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String findName() {
+        return attribute.get("name").toString();
+    }
+
+
+    @Override
+    public String findImage() {
+        return attribute.get("profile_imag").toString();
+    }
+}
--- src/main/java/com/zerobase/plistbackend/module/user/type/RegistrationId.java
+++ src/main/java/com/zerobase/plistbackend/module/user/type/RegistrationId.java
@@ -1,6 +1,7 @@
 package com.zerobase.plistbackend.module.user.type;
 
 import com.zerobase.plistbackend.module.user.dto.response.GoogleResponse;
+import com.zerobase.plistbackend.module.user.dto.response.NaverResponse;
 import com.zerobase.plistbackend.module.user.dto.response.OAuth2Response;
 import lombok.Getter;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -8,7 +9,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 @Getter
 public enum  RegistrationId {
   GOOGLE("Google"),
-  KAKAO("Kakao");
+  KAKAO("Kakao"),
+  NAVER("Naver");
 
   private final String id;
 
@@ -17,11 +19,16 @@ public enum  RegistrationId {
   }
 
   public static OAuth2Response fromId(String id, OAuth2User oAuth2User) {
-    if(id.equalsIgnoreCase(RegistrationId.GOOGLE.getId())) {
+    if (id.equalsIgnoreCase(GOOGLE.id)) {
       return new GoogleResponse(oAuth2User.getAttributes());
-    } else {
-      throw new IllegalArgumentException("Unknown RegistrationId: " + id);
     }
+    if (id.equalsIgnoreCase(NAVER.id)) {
+      return new NaverResponse(oAuth2User.getAttributes());
+    }
+    if (id.equalsIgnoreCase(KAKAO.id)) {
+      // return new KakaoResponse
+    }
+    throw new IllegalArgumentException("Unknown RegistrationId: " + id);
   }
 
   @Override
