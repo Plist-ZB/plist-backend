3c7844a2ae768a3311aad9c9dd5b599f58cb5fdc
= <today6215@gmail.com>
2025-02-21 02:05:38 +0900

    db 저장 로직 대신 redis 저장 로직으로 변경

--- src/main/java/com/zerobase/plistbackend/module/refresh/service/RefreshServiceImpl.java
+++ src/main/java/com/zerobase/plistbackend/module/refresh/service/RefreshServiceImpl.java
@@ -1,21 +1,15 @@
 package com.zerobase.plistbackend.module.refresh.service;
 
 import com.zerobase.plistbackend.module.refresh.dto.NewAccessResponse;
-import com.zerobase.plistbackend.module.refresh.entity.Refresh;
 import com.zerobase.plistbackend.module.refresh.exception.RefreshException;
 import com.zerobase.plistbackend.module.refresh.repository.RefreshRepository;
 import com.zerobase.plistbackend.module.refresh.type.RefreshErrorStatus;
 import com.zerobase.plistbackend.module.user.jwt.JwtUtil;
-import com.zerobase.plistbackend.module.user.repository.UserRepository;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
-import java.sql.Timestamp;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -23,27 +17,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class RefreshServiceImpl implements RefreshService {
 
   private final RefreshRepository refreshRepository;
-  private final UserRepository userRepository;
   private final JwtUtil jwtUtil;
 
-  @Transactional
-  public void addRefreshEntity(String email, String token, Timestamp expired) {
-
-    Optional<Refresh> existingRefresh = refreshRepository.findByUser_UserEmail(email);
-
-    if (existingRefresh.isPresent()) {
-      Refresh refresh = existingRefresh.get();
-      refresh.updateRefreshToken(token, expired);
-      log.info("Updated Refresh token: {}", refresh.getRefreshToken());
-    } else {
-      Refresh refresh = Refresh.builder()
-          .user(userRepository.findByUserEmail(email))
-          .refreshToken(token)
-          .refreshExpiration(expired)
-          .build();
-      refreshRepository.save(refresh);
-      log.info("Created new Refresh token: {}", refresh.getRefreshToken());
-    }
+  public void addRefreshEntity(Long userId, String token) {
+    refreshRepository.saveToken(userId, token);
+    log.info("Created new Refresh token: {}", token);
   }
 
   public void checkRefresh(String refreshToken) {
@@ -77,10 +55,4 @@ public class RefreshServiceImpl implements RefreshService {
     return new NewAccessResponse(jwtUtil.createJwt("access", email, role));
   }
 
-  @Transactional
-  @Scheduled(cron = "0 0 0 * * *")
-  public void refreshCleanup() {
-    Timestamp now = new Timestamp(System.currentTimeMillis());
-    refreshRepository.deleteByRefreshExpirationBefore(now);
-  }
 }
