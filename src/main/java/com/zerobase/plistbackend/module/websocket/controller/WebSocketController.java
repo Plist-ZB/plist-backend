9e8510923cc2e45dfc8254337673f7aba1e3e6dd
chulhyun96 <park82602@gmail.com>
2025-02-24 21:24:53 +0900

    refactor : 클라이언트로부터 받은 메시지를 바로 보내지 않고 Redis를 통해 보낼 수 있도록 리팩토링

--- src/main/java/com/zerobase/plistbackend/module/websocket/controller/WebSocketController.java
+++ src/main/java/com/zerobase/plistbackend/module/websocket/controller/WebSocketController.java
@@ -1,44 +1,45 @@
 package com.zerobase.plistbackend.module.websocket.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zerobase.plistbackend.module.channel.type.ChannelErrorStatus;
 import com.zerobase.plistbackend.module.websocket.domain.VideoSyncManager;
 import com.zerobase.plistbackend.module.websocket.dto.request.ChatMessageRequest;
 import com.zerobase.plistbackend.module.websocket.dto.request.VideoControlRequest;
 import com.zerobase.plistbackend.module.websocket.dto.request.VideoSyncRequest;
-import com.zerobase.plistbackend.module.websocket.dto.response.ChatMessageResponse;
 import com.zerobase.plistbackend.module.websocket.dto.response.VideoControlResponse;
 import com.zerobase.plistbackend.module.websocket.dto.response.VideoSyncResponse;
 import com.zerobase.plistbackend.module.websocket.exception.WebSocketControllerException;
+import com.zerobase.plistbackend.module.websocket.service.RedisChatPubSubService;
 import com.zerobase.plistbackend.module.websocket.service.WebSocketService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "WebSocket API", description = "WebSocket 관련된 API Controller")
 public class WebSocketController {
 
   private final WebSocketService webSocketService;
   private final VideoSyncManager videoSyncManager;
+  private final RedisChatPubSubService redisChatPubSubService;
+  private final ObjectMapper mapper;
 
 
-  @PostMapping("/chat.{channelId}")
-  @SendTo("/sub/chat.{channelId}") // 서버를 거치고 처리한 결과를 전송할 주소
-  public ChatMessageResponse sendMessage(@Payload ChatMessageRequest request) {
-    return webSocketService.sendMessage(request);
+  @MessageMapping("/chat.{channelId}")
+  public void sendMessage(@DestinationVariable Long channelId, @Payload ChatMessageRequest request) throws JsonProcessingException {
+    request.allocateChannelId(channelId);
+    String message = mapper.writeValueAsString(webSocketService.sendMessage(request));
+    redisChatPubSubService.publish("chat", message);
   }
 
   @MessageMapping("/video.{channelId}")
-  @SendTo("/sub/video.{channelId}")
+  @SendTo("/sub/chat.{channelId}")
   public VideoSyncResponse syncVideo(@DestinationVariable Long channelId,
       @Payload VideoSyncRequest request) {
     videoSyncManager.updateCurrentTime(channelId, request.getCurrentTime());
@@ -46,13 +47,13 @@ public class WebSocketController {
   }
 
   @MessageMapping("/enter.{channelId}")
-  @SendTo("/sub/enter.{channelId}")
+  @SendTo("/sub/chat.{channelId}")
   public String enterNewUserForSync() {
     return "NEW_USER_ENTER";
   }
   
   @MessageMapping("/video.control.{channelId}")
-  @SendTo("/sub/video.{channelId}")
+  @SendTo("/sub/chat.{channelId}")
   public VideoControlResponse controlVideo(@DestinationVariable Long channelId,
       @Payload VideoControlRequest request) {
     if (!webSocketService.isHost(channelId, request.getEmail())) {
@@ -62,4 +63,5 @@ public class WebSocketController {
     log.info("호스트가 채널 {}의 비디오 상태를 업데이트: 현재 시간={}", channelId, request.getCurrentTime());
     return new VideoControlResponse(request);
   }
+
 }
