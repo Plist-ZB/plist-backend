b820b389ba2eb0fa958487f906749ed0f7818546
chulhyun96 <park82602@gmail.com>
2025-02-24 21:23:24 +0900

    refactor : @Qualifier 애노테이션 제거

--- src/main/java/com/zerobase/plistbackend/common/app/config/RedisConfig.java
+++ src/main/java/com/zerobase/plistbackend/common/app/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.zerobase.plistbackend.common.app.config;
 
 import com.zerobase.plistbackend.module.websocket.service.RedisChatPubSubService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
