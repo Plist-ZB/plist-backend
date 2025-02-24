a93310ad1d6bb88dd7be22b78b3e81afb5dde2c4
chulhyun96 <park82602@gmail.com>
2025-02-24 22:25:16 +0900

    refactor : 기존 발행 주소 주석 처리, syncVideo 메서드 Redis로 발행 주소 추가

--- src/main/java/com/zerobase/plistbackend/module/websocket/controller/WebSocketController.java
+++ src/main/java/com/zerobase/plistbackend/module/websocket/controller/WebSocketController.java
@@ -11,13 +11,13 @@ import com.zerobase.plistbackend.module.websocket.dto.response.VideoControlRespo
 import com.zerobase.plistbackend.module.websocket.dto.response.VideoSyncResponse;
 import com.zerobase.plistbackend.module.websocket.exception.WebSocketControllerException;
 import com.zerobase.plistbackend.module.websocket.service.RedisChatPubSubService;
+import com.zerobase.plistbackend.module.websocket.service.RedisVideoSyncService;
 import com.zerobase.plistbackend.module.websocket.service.WebSocketService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -28,11 +28,12 @@ public class WebSocketController {
   private final WebSocketService webSocketService;
   private final VideoSyncManager videoSyncManager;
   private final RedisChatPubSubService redisChatPubSubService;
+  private final RedisVideoSyncService redisVideoSyncService;
   private final ObjectMapper mapper;
 
 
   @MessageMapping("/chat.{channelId}")
-//  @SendTo("/sub/chat.{channelId}")
+//        @SendTo("/sub/chat.{channelId}") // 서버를 거치고 처리한 결과를 전송할 주소
   public void sendMessage(@DestinationVariable Long channelId, @Payload ChatMessageRequest request) throws JsonProcessingException {
     request.allocateChannelId(channelId);
     String message = mapper.writeValueAsString(webSocketService.sendMessage(request));
@@ -40,21 +41,22 @@ public class WebSocketController {
   }
 
   @MessageMapping("/video.{channelId}")
-//  @SendTo("/sub/chat.{channelId}")
-  public VideoSyncResponse syncVideo(@DestinationVariable Long channelId,
-      @Payload VideoSyncRequest request) {
+//        @SendTo("/sub/video.{channelId}")
+  public void syncVideo(@DestinationVariable Long channelId,
+      @Payload VideoSyncRequest request) throws JsonProcessingException {
     videoSyncManager.updateCurrentTime(channelId, request.getCurrentTime());
-    return new VideoSyncResponse(request);
+    String syncData = mapper.writeValueAsString(new VideoSyncResponse(request));
+    redisVideoSyncService.publish("videoSync",syncData);
   }
 
   @MessageMapping("/enter.{channelId}")
-//  @SendTo("/sub/chat.{channelId}")
+//     @SendTo("/sub/enter.{channelId}")
   public String enterNewUserForSync() {
     return "NEW_USER_ENTER";
   }
   
   @MessageMapping("/video.control.{channelId}")
-//  @SendTo("/sub/chat.{channelId}")
+//     @SendTo("/sub/video.{channelId}")
   public VideoControlResponse controlVideo(@DestinationVariable Long channelId,
       @Payload VideoControlRequest request) {
     if (!webSocketService.isHost(channelId, request.getEmail())) {
@@ -64,5 +66,4 @@ public class WebSocketController {
     log.info("호스트가 채널 {}의 비디오 상태를 업데이트: 현재 시간={}", channelId, request.getCurrentTime());
     return new VideoControlResponse(request);
   }
-
 }
