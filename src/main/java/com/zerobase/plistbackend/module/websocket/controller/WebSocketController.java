a56f8ae23a3906066f7bd1631ee4c13db20a2b28
chulhyun96 <park82602@gmail.com>
2025-02-24 19:08:58 +0900

    style : Swagger 애노테이션 제거

--- src/main/java/com/zerobase/plistbackend/module/websocket/controller/WebSocketController.java
+++ src/main/java/com/zerobase/plistbackend/module/websocket/controller/WebSocketController.java
@@ -31,25 +31,12 @@ public class WebSocketController {
   private final VideoSyncManager videoSyncManager;
 
 
-  @Operation(
-      summary = "자신과 같은 채널에 속한 인원들 끼리 채팅을 주고받을 수 있습니다.",
-      description = "WebSocket 연결을 통해 메시지를 전송합니다.REST 호출은 지원하지 않습니다."
-          + "-> 클라이언트가 데이터를 서버로 전송할 주소 /chat.{channelId}, 서버를 거치고 처리한 결과를 전송할 주소 /sub/chat.{channelId}"
-  )
   @PostMapping("/chat.{channelId}")
-  @MessageMapping("/chat.{channelId}") // 클라이언트가 메세지를 서버로 전송할 주소
   @SendTo("/sub/chat.{channelId}") // 서버를 거치고 처리한 결과를 전송할 주소
   public ChatMessageResponse sendMessage(@Payload ChatMessageRequest request) {
     return webSocketService.sendMessage(request);
   }
 
-  @Operation(
-      summary = "클라이언트 측에서는 해당 Mapping 주소로 지속적으로 호스트가 보고있는 비디오의 currentTime을 서버로 보냅니다."
-          + "그럼 구독자들은 비디오의 currntTime을 지속적으로 같이 받고 영상 시점을 공유합니다.",
-      description = "WebSocket 연결을 통해 메시지를 전송합니다. REST 호출은 지원하지 않습니다."
-          + "-> 클라이언트가 데이터를 서버로 전송할 주소 /video.{channelId}, 서버를 거치고 처리한 결과를 전송할 주소 /sub/video.{channelId}"
-  )
-  @PostMapping("/video.{channelId}")
   @MessageMapping("/video.{channelId}")
   @SendTo("/sub/video.{channelId}")
   public VideoSyncResponse syncVideo(@DestinationVariable Long channelId,
@@ -58,24 +45,12 @@ public class WebSocketController {
     return new VideoSyncResponse(request);
   }
 
-  @Operation(
-      summary = "클라이언트 측에서 지속적으로 받은 호스트의 비디오 currentTime을 새로운 사용자가 입장시 호스트의 시점으로 같이 비디오가 동기화됩니다.",
-      description = "WebSocket 연결을 통해 메시지를 전송합니다. REST 호출은 지원하지 않습니다."
-          + "-> 클라이언트가 데이터를 서버로 전송할 주소 /join.{channelId}, 서버를 거치고 처리한 결과를 전송할 주소 /sub/video.{channelId}"
-  )
-  @PostMapping("/enter.{channelId}")
   @MessageMapping("/enter.{channelId}")
   @SendTo("/sub/enter.{channelId}")
   public String enterNewUserForSync() {
     return "NEW_USER_ENTER";
   }
   
-  @Operation(
-      summary = "현재 재생중인 비디오의 재생 및 일시정지를 호스트만 요청할 수 있고 재생 및 일시정지의 시점을 사용자 모두 동기화 합니다.",
-      description = "WebSocket 연결을 통해 메시지를 전송합니다. REST 호출은 지원하지 않습니다."
-          + "-> 클라이언트가 데이터를 서버로 전송할 주소 /video.control.{channelId}, 서버를 거치고 처리한 결과를 전송할 주소 /sub/video.{channelId}"
-  )
-  @PostMapping("/video.control.{channelId}")
   @MessageMapping("/video.control.{channelId}")
   @SendTo("/sub/video.{channelId}")
   public VideoControlResponse controlVideo(@DestinationVariable Long channelId,
@@ -86,14 +61,5 @@ public class WebSocketController {
     videoSyncManager.updateCurrentTime(channelId, request.getCurrentTime());
     log.info("호스트가 채널 {}의 비디오 상태를 업데이트: 현재 시간={}", channelId, request.getCurrentTime());
     return new VideoControlResponse(request);
-  }
-  @Operation(
-      summary = "채널의 플레이리스트에서 변경 감지 시 작동, 채널에 속하는 플레이리스트의 최신 DB정보를 가져와 참여자들에게 전달",
-      description = "해당 엔드포인트는 /channel/{channelId}/add-video, /channel/{channelId}/update-video, /channel/{channelId}/delete-video 호출 시 작동"
-          + "결과를 전송할 엔드포인트 /sub/video.{channelId}"
-  )
-  @PostMapping("/trigger")
-  public void emptyVoid () {
-    // Swagger 문서화를 위한 메서드 동작 X
   }
 }
