ce7ed5bf46d353dfd7e4d78d261744de0217fcf1
chulhyun96 <park82602@gmail.com>
2025-02-26 12:09:06 +0900

    style : 로그 추가

--- src/main/java/com/zerobase/plistbackend/module/websocket/service/RedisChatPubSubService.java
+++ src/main/java/com/zerobase/plistbackend/module/websocket/service/RedisChatPubSubService.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zerobase.plistbackend.module.websocket.dto.response.ChatMessageResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisChatPubSubService implements MessageListener {
@@ -28,6 +30,7 @@ public class RedisChatPubSubService implements MessageListener {
         try {
             ChatMessageResponse response = mapper.readValue(body, ChatMessageResponse.class);
             messagingTemplate.convertAndSend("/sub/chat." + response.getChannelId(), response);
+            log.info("Message received: " + body);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
