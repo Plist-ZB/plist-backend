1c521db0d067f26968f7b98cbe0079c2eefd203c
HAKSUYOON <hakssu93@gmail.com>
2025-03-06 11:37:24 +0900

    style: 사용하지 않는 import 문 삭제

--- src/main/java/com/zerobase/plistbackend/module/channel/controller/ChannelController.java
+++ src/main/java/com/zerobase/plistbackend/module/channel/controller/ChannelController.java
@@ -16,8 +16,6 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
