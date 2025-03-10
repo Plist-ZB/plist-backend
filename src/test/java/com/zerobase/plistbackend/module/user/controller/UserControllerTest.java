0de569b781c0b08864a016c2a16e83b120bc266e
chulhyun96 <park82602@gmail.com>
2025-03-10 17:18:55 +0900

    test : Playtime UserController 단위 테스트 코드 작성

---
+++ src/test/java/com/zerobase/plistbackend/module/user/controller/UserControllerTest.java
@@ -0,0 +1,79 @@
+package com.zerobase.plistbackend.module.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.plistbackend.module.user.dto.response.PlayTimeResponse;
+import com.zerobase.plistbackend.module.user.model.auth.CustomOAuth2User;
+import com.zerobase.plistbackend.module.user.service.UserServiceImpl;
+import com.zerobase.plistbackend.module.user.util.TimeValueFormatter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserServiceImpl userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원의 플리방 이력 정보 가져오기 API 테스트")
+    void getPlaytime() throws Exception {
+        // given
+        int year = 2022;
+        Long userId = 123L;
+
+        List<PlayTimeResponse> playTimeResponses = new ArrayList<>();
+
+        Timestamp startDate = Timestamp.valueOf(LocalDateTime.now().withYear(year).withMonth(1));
+        Timestamp endDate = Timestamp.valueOf(LocalDateTime.now().withYear(year + 1).withMonth(1));
+        String time = TimeValueFormatter.formatToString(startDate, endDate);
+
+        PlayTimeResponse playTimeResponse = new PlayTimeResponse(time, 50, 0L);
+        playTimeResponses.add(playTimeResponse);
+
+        CustomOAuth2User dummyUser = mock(CustomOAuth2User.class);
+        given(dummyUser.findId()).willReturn(userId);
+
+        given(userService.getPlaytime(userId, year)).willReturn(playTimeResponses);
+
+
+        // when & then
+        mockMvc.perform(get("/v3/api/me/playtime")
+                        .param("year", String.valueOf(year))
+                        .with(authentication(new TestingAuthenticationToken(dummyUser, null, "ROLE_USER"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].totalPlayTime").value("8760시간 0분"))
+                .andExpect(jsonPath("$[0].totalParticipant").value(50))
+                .andExpect(jsonPath("$[0].totalFollowers").value(0))
+                .andDo(print());
+
+    }
+}
