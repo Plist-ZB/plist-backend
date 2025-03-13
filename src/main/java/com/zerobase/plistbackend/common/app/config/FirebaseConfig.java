73c6eba74d7cb474d1337c747c06468359897132
HAKSUYOON <hakssu93@gmail.com>
2025-03-13 14:36:16 +0900

    chore : FCM 세팅

---
+++ src/main/java/com/zerobase/plistbackend/common/app/config/FirebaseConfig.java
@@ -0,0 +1,24 @@
+package com.zerobase.plistbackend.common.app.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+
+  public FirebaseMessaging firebaseMessaging() throws IOException {
+    FileInputStream serviceAccount = new FileInputStream(
+        "src/main/resources/firebase-service-account.json");
+
+    FirebaseOptions options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+
+    FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
+    return FirebaseMessaging.getInstance(firebaseApp);
+  }
+}
