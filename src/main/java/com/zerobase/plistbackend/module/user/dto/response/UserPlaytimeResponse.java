5668283189ff4df531ebad2afac8158b0dc744b1
chulhyun96 <park82602@gmail.com>
2025-03-19 18:38:56 +0900

    feat : 회원의 플립방 이력 정보 가져오기

--- src/main/java/com/zerobase/plistbackend/module/user/dto/response/UserPlaytimeResponse.java
+++ src/main/java/com/zerobase/plistbackend/module/user/dto/response/UserPlaytimeResponse.java
@@ -1,10 +1,19 @@
 package com.zerobase.plistbackend.module.user.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UserPlaytimeResponse {
-    private Long totalPlayTime;
+    private String totalPlayTime;
+
+    @Builder
+    private UserPlaytimeResponse(String totalPlayTime) {
+        this.totalPlayTime = totalPlayTime;
+    }
+    public static UserPlaytimeResponse from(String totalPlayTime) {
+        return new UserPlaytimeResponse(totalPlayTime);
+    }
 }
