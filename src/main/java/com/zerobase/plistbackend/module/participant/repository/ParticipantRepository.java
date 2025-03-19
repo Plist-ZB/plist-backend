5c36ddfee98281fd50cbca9d54ce666b4a7b8a14
chulhyun96 <park82602@gmail.com>
2025-03-19 17:11:11 +0900

    feat : findByUserIdAndDate 기능 추가

--- src/main/java/com/zerobase/plistbackend/module/participant/repository/ParticipantRepository.java
+++ src/main/java/com/zerobase/plistbackend/module/participant/repository/ParticipantRepository.java
@@ -3,14 +3,27 @@ package com.zerobase.plistbackend.module.participant.repository;
 import com.zerobase.plistbackend.module.channel.entity.Channel;
 import com.zerobase.plistbackend.module.participant.entity.Participant;
 import com.zerobase.plistbackend.module.user.entity.User;
-import java.util.List;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 
-  boolean existsByUser(User user);
+    boolean existsByUser(User user);
 
-  List<Participant> findByChannel(Channel channel);
+    List<Participant> findByChannel(Channel channel);
 
-  Participant findByUser(User user);
+    Participant findByUser(User user);
+
+    @Query(
+            "select p from Participant p where p.user.userId = :userId and p.participantCreatedAt >= :startDate and p.participantCreatedAt < :endDate"
+    )
+    List<Participant> findByUserIdAndDate(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 }
