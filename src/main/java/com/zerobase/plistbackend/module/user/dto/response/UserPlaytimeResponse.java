362c85322bac7853b0c49261593d39c08b8f8d16
chulhyun96 <park82602@gmail.com>
2025-03-21 00:13:36 +0900

    refactor : final 필드로 변경

--- src/main/java/com/zerobase/plistbackend/module/user/dto/response/UserPlaytimeResponse.java
+++ src/main/java/com/zerobase/plistbackend/module/user/dto/response/UserPlaytimeResponse.java
@@ -2,17 +2,16 @@ package com.zerobase.plistbackend.module.user.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class UserPlaytimeResponse {
-    private String totalPlayTime;
+    private final String totalPlayTime;
 
     @Builder
     private UserPlaytimeResponse(String totalPlayTime) {
         this.totalPlayTime = totalPlayTime;
     }
+
     public static UserPlaytimeResponse from(String totalPlayTime) {
         return new UserPlaytimeResponse(totalPlayTime);
     }
