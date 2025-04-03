68534ea56fceef4460007712ff882dad406277f5
HAKSUYOON <hakssu93@gmail.com>
2025-04-03 22:41:20 +0900

    fix : FileInputStream 의 경로가 JAR 파일에서는 인식을 하지못해서 ClassPathResource를 통해 firebase 설정 파일 불러오도록 변경

--- src/main/java/com/zerobase/plistbackend/common/app/config/FirebaseConfig.java
+++ src/main/java/com/zerobase/plistbackend/common/app/config/FirebaseConfig.java
@@ -4,18 +4,19 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class FirebaseConfig {
 
   @Bean
   public FirebaseMessaging firebaseMessaging() throws IOException {
-    FileInputStream serviceAccount = new FileInputStream(
-        "src/main/resources/firebase-service-account.json");
+    InputStream serviceAccount = new ClassPathResource(
+        "firebase-service-account.json").getInputStream();
 
     FirebaseOptions options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
